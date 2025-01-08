eafe816f27b59feb2b04ae3dd2576f88531f5169
abhishek <abhishekjaswal1122@gmail.com>
2025-01-08 14:06:35 +0530

    add new files to jsnotes folder

---
+++ script.js
@@ -0,0 +1,21 @@
+var quiz = {
+    'question1': {
+        'question': 'color of sky',
+        '1': 'blue',
+        '2': 'green',
+        '3': 'red'
+    },
+    'president of usa': {
+        '1': 'donald',
+        '2': 'barack',
+        '3': 'george'
+    }
+}
+
+
+var answers = {
+    '1': 'blue',
+    '2': 'donald',
+}
+
+console.log(quiz["president of usa"]);
